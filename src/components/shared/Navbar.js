32af0ae575681d17a4a1b2d32afdd543cba78763
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-12-26 07:45:40 +0600

    Navbar added

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -5,10 +5,10 @@ import Link from 'next/link';
 const Navbar = () => {
    
     return (
-        <div className="max-w-6xl mx-auto navbar">
+        <div className="max-w-6xl mx-auto py-5 navbar">
             <div className="navbar-start">
 
-                <a className=" font-bold text-3xl">Munchies</a>
+                <a className=" font-bold text-3xl">Simple Mart</a>
             </div>
             <div className="navbar-end gap-4">
                 <Link href='/' className="btn">Home</Link>
