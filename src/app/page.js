28f58846f9895535e2956e9ed5c40110601646d0
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-12-26 18:04:26 +0600

    All products added to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Carousel from '@/components/ui/Carousel'
+import Products from '@/components/products/Products';
 const Homepage = () => {
   return (
     <div className='max-w-6xl min-h-screen mx-auto'>
       <Carousel></Carousel>
+      <Products></Products>
     </div>
   );
 };
